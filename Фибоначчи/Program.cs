122a7765510b0b9e9075a2c521568b8e91d6e20c
Alexandr <alshumkov2@gmail.com>
2021-11-24 15:23:12 +0300

    Фибоначчи

---
+++ Фибоначчи/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*int Fib(int n)
+{
+    if (n = 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    else return Fib(n - 1) + Fib(n - 2);
+}
+
+
+    Console.Write(Fib(6));*/
+
+
+
+
+
+/*int Fib()
+{
+    int f=0,f1=1;
+    for(int i=0;i<11;i++)
+    {
+        f=f+f1;
+        Console.Write($"{f} ");
+        f1=f-f1;
+    }
+    return f;
+}
+Console.WriteLine(Fib());*/
