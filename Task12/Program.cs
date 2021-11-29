00912e8e615dfd9d5ede4fb31c8e1a656337c8e3
Alexandr <alshumkov2@gmail.com>
2021-11-29 19:20:17 +0300

    12. Дан целочисленный двумерный массив, размерности n х m. Найти количество элементов кратных 7.

---
+++ Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿// 12. Дан целочисленный двумерный массив, размерности n х m. Найти количество элементов кратных 7.
+
+int[,] arr = new int[5, 5];
+Random rnd = new Random();
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = rnd.Next(1, 100);
+        Console.Write($"{arr[i, j],4}");
+
+    }
+        Console.WriteLine();
+}
+int count = 0;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (arr[i, j] % 7 == 0)
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine(count);
